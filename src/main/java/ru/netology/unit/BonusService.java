8858854349579a695803f36481755738ee465940
aleks skudarnov <skudarnovfred@mail.ru>
2023-02-02 19:47:09 +0500

    README changed

--- src/main/java/ru/netology/unit/BonusService.java
+++ src/main/java/ru/netology/unit/BonusService.java
@@ -1,14 +1,13 @@
 package ru.netology.unit;
 
 public class BonusService {
-    public int calculateBonus (int amount) {
-        if (amount < 1000) {
-            return 0;
-        }
-        int bonus = (amount - 1000) / 100;
-                if (bonus > 100) {
-                    return 100;
-                }
-                return bonus;
+
+    private final int boundary = 1000;
+
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
+
+
 }
