060409f10cf8c3f49dec1d8251c2a64eb1e36018
aleks skudarnov <skudarnovfred@mail.ru>
2023-02-03 17:32:54 +0500

    README changed

---
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.unit.BonusService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+    @Test
+    void remainTest2() {
+        BonusService service = new BonusService();
+        int expected = 100;
+        int amount = 900;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void remainTest3() {
+        BonusService service = new BonusService();
+        int expected = 1100;
+        int amount = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+}
